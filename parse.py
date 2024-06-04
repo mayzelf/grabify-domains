fdb71b8a58ef0e9bd7bebba729b27ca22bb78eb1
justno <120106323+mayzelf@users.noreply.github.com>
2024-06-04 10:47:56 +0200

    added domains-only list

--- parse.py
+++ parse.py
@@ -14,13 +14,19 @@ dropdown_items = soup.find_all('a', class_='dropdown-item')
 
 # Extract the text from the 'a' tag and the 'small' tag within it
 result = []
+cleared = []
 for item in dropdown_items:
     a_text = next(item.children).strip() if isinstance(next(item.children), NavigableString) else ''
     small_text = item.small.get_text(strip=True) if item.small else ''
     result.append(f"{a_text} {small_text}")
+    cleared.append(a_text)
 
 print(result)
 
 with open('domains.txt', 'w') as file:
     for item in result:
+        file.write(item + '\n')
+
+with open('domains-only.txt', 'w') as file:
+    for item in cleared:
         file.write(item + '\n')
